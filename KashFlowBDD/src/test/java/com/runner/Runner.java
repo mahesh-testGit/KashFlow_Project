38259789bb1fa249cedf3555833e1cfef75bcb5b
maheshwag9090 <maheshwag9090@gmail.com>
2025-12-12 13:20:37 +0530

    Added Report folder to project

--- KashFlowBDD/src/test/java/com/runner/Runner.java
+++ KashFlowBDD/src/test/java/com/runner/Runner.java
@@ -5,6 +5,20 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/kashflow.test.features/VinaykBank.feature",glue={"come.stepDefinations"})
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-report",
+                "json:target/cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml"
+        },
+        features = "src/test/resources/kashflow.test.features/VinaykBank.feature",
+        glue = "come.stepDefinations"
+)
 public class Runner {
 }
+
+
+
+
+
